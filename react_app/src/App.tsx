ff4d93f730432251fba0b6e6c7856258137e6dc1
omkarmeher42 <omkarmeher42@gmail.com>
2024-01-27 20:03:18 +0530

    local

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/welcome/" element={<Welcome user="Omkar" />} />
+            <Route path="/welcome/" element={<Welcome user="Omkar Local" />} />
             <Route path="/upload/" element={<UploadDocument />} />
             <Route path="/dashboard/" element={<Dashboard />} />
             <Route path="/contact/" element={<ContactUs />} />
